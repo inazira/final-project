ba17eaf52a3abbb274c355dedbbd3cb6509188ab
inazira <140095967+inazira@users.noreply.github.com>
2024-03-31 17:57:13 +0200

    Some updates

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Homepage
-    </div>
+    <nav>
+       <ul>
+    <li><a href="/home">Homepage</a></li>
+    <li><a href="/about">About Us</a></li>
+    <li><a href="/blog">Blog</a></li>
+  </ul>
+  <header>Little Lemon Restaurant</header>
+  <a href="/book" role="button">Book a table</a>
+  <footer>All copyrights</footer>
+  </nav>
   );
 }
 
